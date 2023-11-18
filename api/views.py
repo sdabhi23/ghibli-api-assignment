666b815caf72630004bd6da3ed2becdb5c5feba2
shrey <shrey.dabhi23@gmail.com>
2023-11-18 11:59:29 +0530

    format with black formatter

--- api/views.py
+++ api/views.py
@@ -2,10 +2,11 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 
-@api_view(http_method_names=['GET'])
+@api_view(http_method_names=["GET"])
 def root(request):
     return Response({"message": "Hello, world!"})
 
-@api_view(http_method_names=['GET'])
+
+@api_view(http_method_names=["GET"])
 def ghibli_api(request):
     return Response({"message": "Hello, world!"})
